001d20c5ae0156522fe4dea13d8834001ad0e74b
beandrej <beandrej@ethz.ch>
2024-04-11 18:03:53 +0200

    add source

--- EnergyHub.py
+++ EnergyHub.py
@@ -9,10 +9,10 @@
     ========================================================================
     Parameter sources and assumptions:
     -> Demands:
-    - Heat Demand (onyl for cooking and showering): Heat demand was taken from renewables.ninja with choosing the coordinates
+    - Heat Demand (onyl for cooking and showering): Heat demand was taken from renewables.ninja and scaled to be around 10% of the electricity demand: https://www.consumersenergy.com/community/sustainability/our-hometown-stories/energy-efficient-cooking#:~:text=According%20to%20the%20U.S.%20Department,use%20occurs%20in%20the%20kitchen
     - Electricity Demand (per month average household consumption at 170 kWh): Electricity demand for brazil was very hard to find, so we took a electricity profile of the US and 
                                                                                 and scaled it accordingly so the energy consumption matches 170kWh per household, which we found to be the monthly energy consumption in favelas in Brazil (see pitch document of our idea).
-
+                                                                                https://www.cnet.com/home/energy-and-utilities/what-size-home-battery-do-i-need/
     
     - Electricity Mix Emission in Brazil (kgCO2/kWh): https://www.climatiq.io/data/emission-factor/2ac52a91-5922-4f9f-8def-f4302f4ecf55
 
@@ -23,8 +23,7 @@
     - Area of favela: https://www.citypopulation.de/en/brazil/rio/_/33045570538__cidade_de_deus/
     - Area of favela that is covered by roofs (%): https://isprs-annals.copernicus.org/articles/IV-2-W5/437/2019/isprs-annals-IV-2-W5-437-2019.pdf (page 443) -> gives information of how densily houses are built in favelas -> thus how much of the total area is covered by roofs -> roughly 60%
     - Percenatage of roof area that can be covered by (flat) PV panels: https://www.nrel.gov/docs/fy14osti/60593.pdf -> roughly 50% (see page 6)
-    - Maxmimum Capacity of Batteries that can be installed: Per household maximum available battery size: ->
-
+    - Maxmimum Capacity of Batteries that can be installed: Per household maximum available battery size: -> https://www.cnet.com/home/energy-and-utilities/what-size-home-battery-do-i-need/
     -> Investment:
     - Average Government Expenses in energy per person in Brazil: https://www.gov.br/en/government-of-brazil/latest-news/2021/brazil-is-targeting-extensive-energy-investments#:~:text=According%20to%20official%20data%20from,and%20new%20sources%20of%20energy. -> Value given is total money spent -> divided by total population and then multiplied by population of favela
 
@@ -37,6 +36,7 @@
 
     elec emissions:		https://www.carbonfootprint.com/docs/2023_02_emissions_factors_sources_for_2022_electricity_v10.pdf
 
+    feed in price:      https://www.roedl.com/renewable-energy-consulting/markets/countries/marketing-models-brazil#:~:text=Differently%20from%20some%20developed%20countries,used%20is%20%E2%80%9CNet%20Metering%E2%80%9D.
 """
 
 import numpy as np
